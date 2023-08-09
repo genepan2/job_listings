fe1bced603f377056a8d9d8b001f5d25ec0641c4
Levi da Silva <37868042+leviGab001@users.noreply.github.com>
2023-08-09 13:22:13 +0200

    Update themuse-job-search.py

--- themuse-job-search.py
+++ themuse-job-search.py
@@ -1,11 +1,26 @@
-import requests  # For making HTTP requests to APIs and websites
-import json      # For working with JSON data
-import re        # For working with regular expressions
-import unicodedata  # For handling Unicode characters and normalization
-
+import requests  # Import the 'requests' module to make HTTP requests to APIs and websites
+import json  # Import the 'json' module to work with JSON data, such as parsing and serializing
+import re  # Import the 're' module for working with regular expressions, useful for text manipulation
+import unicodedata  # Import the 'unicodedata' module for working with Unicode characters and normalization
 
 # Function to make a GET request and handle exceptions
 def get_requests_result(url, page_number):
+    """
+    Make a GET request to the specified URL and handle exceptions.
+
+    Parameters:
+    url : str
+        The URL to make the GET request to.
+    page_number : int
+        The page number to append to the URL.
+
+    Returns:
+    requests.Response
+        The response object from the HTTP request.
+    Raises:
+    Exception
+        If the request or response encounter an error.
+    """
     try:
         res = requests.get(url + str(page_number))
         res.raise_for_status()
@@ -13,20 +28,50 @@ def get_requests_result(url, page_number):
     except requests.exceptions.RequestException as e:
         raise Exception(f"Requesting page {page_number} failed: {e}")
 
-
 # Function to extract JSON content from response
 def get_json_content(res):
-    return res.json()['results']
+    """
+    Extract JSON content from the response object.
 
+    Parameters:
+    res : requests.Response
+        The response object from the HTTP request.
+
+    Returns:
+    dict
+        The extracted JSON content.
+    """
+    return res.json()['results']
 
 # Function to strip HTML tags from text
 def strip_html_tags(text):
+    """
+    Remove HTML tags from the given text using regular expressions.
+
+    Parameters:
+    text : str
+        The input text containing HTML tags.
+
+    Returns:
+    str
+        The input text with HTML tags removed.
+    """
     clean_text = re.sub(r'<.*?>', '', text)
     return clean_text
 
-
 # Function to translate Unicode characters to ASCII
 def translate_unicode_characters(text):
+    """
+    Translate Unicode characters to ASCII or remove them.
+
+    Parameters:
+    text : str
+        The input text containing Unicode characters.
+
+    Returns:
+    str
+        The input text with Unicode characters translated to ASCII.
+    """
     translated_text = ''.join(
         char if ord(char) < 128 else unicodedata.normalize('NFKD', char).encode('ascii', 'ignore').decode('utf-8')
         for char in text
@@ -36,11 +81,24 @@ def translate_unicode_characters(text):
 
 # Function to remove specified fields from JSON object
 def remove_some_tags(obj):
+    """
+    Recursively remove specified fields from a JSON-like object.
+
+    This function removes the specified fields 'short_name', 'id', 'model_type', 'tags', and 'type'
+    from a JSON-like object. It operates recursively, traversing through dictionaries and lists.
+
+    Parameters:
+    obj : dict or list
+        The JSON-like object to remove fields from.
+
+    Returns:
+    None
+    """
     if isinstance(obj, dict):
         obj.pop('short_name', None)
         obj.pop('id', None)
         obj.pop('model_type', None)
-        obj.pop('tag', None)
+        obj.pop('tags', None)
         obj.pop('type', None)
         for key, value in obj.items():
             remove_some_tags(value)
