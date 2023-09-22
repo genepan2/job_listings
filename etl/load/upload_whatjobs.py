8e630a5398c2fd7d644b466f4b344382f6ae0159
Levi da Silva <levigab001@gmail.com>
2023-09-22 17:24:09 +0200

    Fixed paths + Deleted settings, no need anymore

--- etl/load/upload_whatjobs.py
+++ etl/load/upload_whatjobs.py
@@ -1,14 +1,13 @@
 import settings  # Importing settings will also update the system path
 
-from mongodb_connection import MongoDBUploader
+from config.mongodb_connection import MongoDBUploader
 
-def upload_whatjobs():
-    whatjobs_collection_name = "whatjobs_jobs_collected"
-    
-    uploader_whatjobs = MongoDBUploader("job_listing_db", whatjobs_collection_name)
-    uploader_whatjobs.upload_json_file(settings.WHATJOBS_FILE_PATH)  # Use the path from settings
-    uploader_whatjobs.close()
-
-# Run upload_whatjobs function if this script is executed
-if __name__ == "__main__":
-    upload_whatjobs()
+class WhatjobsUploader:
+    @staticmethod
+    def upload():
+        whatjobs_file_path = "data/processed/whatjobs_json_files/whatjobs_cleaned_data.json"
+        whatjobs_collection_name = "whatjobs_jobs_collected"
+        
+        uploader = MongoDBUploader("job_listing_db", whatjobs_collection_name)
+        uploader.upload_json_file(whatjobs_file_path)
+        uploader.close()
--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 # Import extraction, transformation, and loading functions from their respective locations in the project structure
-import settings 
-
 from etl.extract.whatjobs_extractor import WhatjobsDataExtractor
 from etl.transform.whatjobs_transformer import WhatjobsDataTransformer
-from etl.load.upload_whatjobs import upload_whatjobs
+from etl.load.upload_whatjobs import WhatjobsUploader
 
 from etl.extract.themuse_extractor import ThemuseDataExtractor
 from etl.transform.themuse_transformer import ThemuseDataTransformer
-from etl.load.upload_themuse import upload_themuse
+from etl.load.upload_themuse import ThemuseUploader
 
 from etl.extract.linkedin_extractor import JobSearchLinkedInExtractor
 from etl.transform.linkedin_transformer import JobSearchLinkedInTransformer
-from etl.load.upload_linkedin import upload_linkedin
+from etl.load.upload_linkedin import LinkedinUploader
 
-from src.utils.merge_to_all_jobs_list import merge_collections_to_all_jobs_list
+from utils.merge_collections import MergeCollections
+
 
 def main():
     # Extraction and transformation for WhatJobs
@@ -44,12 +43,12 @@ def main():
     linkedin_transformer.run_all()
 
     # Uploading to MongoDB
-    upload_whatjobs()
-    upload_themuse()
-    upload_linkedin()
+    WhatjobsUploader.upload()
+    ThemuseUploader.upload()
+    LinkedinUploader.upload()
 
     # Merging collections in MongoDB
-    merge_collections_to_all_jobs_list()
+    MergeCollections.merge_to_all_jobs_list()
 
 if __name__ == "__main__":
     main()
