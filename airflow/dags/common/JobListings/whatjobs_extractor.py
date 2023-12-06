94192edb6ec132783de9b2a63cef38b226fcc640
Levi Gabriel da Silva <37868042+leviGab001@users.noreply.github.com>
2023-12-06 22:15:58 +0100

    Added The Muse DAG

---
+++ airflow/dags/common/JobListings/whatjobs_extractor.py
@@ -0,0 +1,159 @@
+import json
+import os
+import requests
+from bs4 import BeautifulSoup
+import unicodedata
+from datetime import datetime
+from config.constants import FIELDS
+
+NUM_PAGES_TO_SCRAPE = 1  # The number of pages to be scraped from each search result
+
+class WhatjobsDataExtractor:
+    """
+    Class responsible for extracting job listing data from WhatJobs based on provided job title and location.
+    """
+    global_job_number = 1   # Class level attribute to ensure each job gets a unique identifier
+    all_jobs = []  # Class level attribute to store all jobs across different job titles and locations
+
+    def __init__(self, job_title, location, items=None):
+        """
+        Initialize the extractor with job title and location.
+
+        Parameters:
+        - job_title (str): The job title to search for.
+        - location (str): The location to search within.
+        - items (int, optional): The number of items to scrape. Defaults to None, meaning all items will be scraped.
+        """
+        self.job_title = job_title
+        self.location = location
+        self.items = items  # The number of items to scrape on each page
+        self.base_url = f"https://de.whatjobs.com/jobs/{self.job_title}/{self.location}"  # The base URL for the search query
+        self.num_pages_to_scrape = NUM_PAGES_TO_SCRAPE  # The number of pages to scrape for each search query
+        self.output_filename = "backend/app/data/raw/whatjobs_json_files/whatjobs_raw_data.json"  # The file where the scraped data will be stored
+
+        # Create a directory for saving the JSON files, if it doesn't already exist
+        directory_path = "backend/app/data/raw/whatjobs_json_files"
+        if not os.path.exists(directory_path):
+            os.makedirs(directory_path)
+
+        # If this is the first instance and a previous output file exists, reset the all_jobs list for new data
+        if WhatjobsDataExtractor.global_job_number == 1 and os.path.exists(self.output_filename):
+            WhatjobsDataExtractor.all_jobs = []
+
+    def scrape_page(self, url):
+        """
+        Scrape job data from a single page of WhatJobs.
+
+        Parameters:
+        - url (str): The URL of the page to scrape.
+
+        Returns:
+        - list: A list of dictionaries containing job data.
+        """
+        # Sending a request to the URL, and getting the content
+        response = requests.get(url)
+        # Using BeautifulSoup to parse the HTML content
+        soup = BeautifulSoup(response.content, 'html.parser')
+        # Finding all job listings on the page
+        job_listings = soup.find_all("div", class_="searchResultItem")
+        # If the number of items to scrape is specified, slice the list to get the required number of items
+        job_listings = job_listings[:self.items] if self.items else job_listings
+        jobs_data = []  # A list to store the job data dictionaries
+
+        # Loop through all job listings to extract the job data
+        for job in job_listings:
+            # Extracting various data elements from the job listing
+            title_element = job.find("h2", class_="title")
+            title = title_element.text.strip() if title_element else "N/A"
+
+            location_element = job.find("div", class_="posR")
+            location = location_element.text.split(' ', 1)[1].strip() if location_element else "N/A"
+
+            company_element = job.find("span", class_="wjIcon24 companyName")
+            company = company_element.find_parent('div').text.strip() if company_element else "N/A"
+
+            date_published_element = job.find("span", class_="wjIcon24 jobAge")
+            date_published = date_published_element.find_parent('div').text.strip() if date_published_element else "N/A"
+
+            description_element = job.find("span", class_="jDesc")
+            job_url = description_element.find_next('a')['href'] if description_element else "N/A"
+            full_description = self.get_full_description(job_url)  # Get the full description using the URL
+
+            # Collating the extracted data into a dictionary
+            job_data = {
+                #FIELDS["number"]: f"whatjobs-{WhatjobsDataExtractor.global_job_number}",
+                FIELDS["company_name"]: company,
+                FIELDS["title"]: title,
+                FIELDS["location"]: location,
+                FIELDS["publish_date"]: date_published,
+                FIELDS["description"]: full_description,  # Updated to include the full description
+                FIELDS["url"]: job_url,
+                FIELDS["search_datetime"]: datetime.now().isoformat(),
+                FIELDS["search_keyword"]: self.job_title,
+                FIELDS["search_location"]: location
+            }
+
+            # Adding the job data dictionary to the jobs_data list
+            jobs_data.append(job_data)
+            WhatjobsDataExtractor.global_job_number += 1  # Incrementing the global job number
+
+        return jobs_data  # Returning the list of job data dictionaries
+
+    def scrape_all_pages(self):
+        """
+        Scrape job data from all pages of WhatJobs based on the initialized job title and location.
+        """
+        # Loop through all the pages specified by num_pages_to_scrape
+        for page in range(1, self.num_pages_to_scrape + 1):
+            page_url = f"{self.base_url}?page={page}"  # Forming the URL for each page
+            jobs_on_page = self.scrape_page(page_url)  # Scraping the job data from the page
+
+            # If no job data is returned, break the loop
+            if not jobs_on_page:
+                break
+
+            # Otherwise, extend the all_jobs list with the job data from the page
+            WhatjobsDataExtractor.all_jobs.extend(jobs_on_page)
+
+        # Save the aggregated jobs data to a JSON file
+        with open(self.output_filename, "w") as outfile:
+            json.dump(WhatjobsDataExtractor.all_jobs, outfile, ensure_ascii=False, indent=4)
+        print(f"Saved all {len(WhatjobsDataExtractor.all_jobs)} jobs in '{self.output_filename}'.")
+
+    def get_full_description(self, url):
+        """
+        Fetch the full job description from a given URL.
+
+        Parameters:
+        - url (str): The URL of the job listing.
+
+        Returns:
+        - str: The full job description.
+        """
+        # Sending a request to the job URL
+        response = requests.get(url)
+        # Using BeautifulSoup to parse the HTML content
+        soup = BeautifulSoup(response.content, 'html.parser')
+        # Finding the job description div
+        description_div = soup.find("div", class_="dDesc")
+
+        # If the description div is found, extract and return the text content
+        if description_div:
+            description = description_div.get_text(separator=' ', strip=True)
+            description = description.replace("\n", " ")
+            description = unicodedata.normalize("NFC", description)
+            return description
+        else:
+            print(f"Description not found for URL: {url}")
+            return "N/A"  # Return "N/A" if the description is not found
+
+if __name__ == "__main__":
+    # List of job titles and locations to search for
+    job_titles = ["Data", "Data Engineer", "Big Data Engineer", "Data Scientist","Data Analist", "Business Intelligence Engineer", "Machine Learning Engineer"]
+    locations = ["berlin--berlin", "cologne", "hamburg--hamburg", "munich", "rankfurt"]
+
+    # For each job title and location combination, scrape the job listings
+    for title in job_titles:
+        for loc in locations:
+            scraper = WhatjobsDataExtractor(title, loc)
+            scraper.scrape_all_pages()
---
+++ backend/app/etl/transform/themuse_transformer copy.py
@@ -0,0 +1,147 @@
+import json
+import re
+import unicodedata
+from datetime import datetime
+import os
+from config.constants import FIELDS
+from langdetect import detect
+
+class ThemuseDataTransformer:
+    """
+    A class to handle the transformation of job data extracted from TheMuse API.
+    This includes removing HTML tags, translating Unicode characters, and reformatting the data.
+
+    Attributes:
+        directory_path (str): Directory where the raw data is located.
+        processed_directory_path (str): Directory where the transformed data will be saved.
+        job_number (int): A counter used for numbering the transformed jobs.
+    """
+
+    def __init__(self):
+        """Initialize the ThemuseDataTransformer with predefined settings."""
+        self.directory_path = "backend/app/data/raw/themuse_json_files"
+        self.processed_directory_path = "backend/app/data/processed/themuse_json_files"
+        self.job_number = 1
+
+        # Ensure the processed directory exists
+        if not os.path.exists(self.processed_directory_path):
+            os.makedirs(self.processed_directory_path)
+
+    def strip_html_tags(self, text):
+        """
+        Remove HTML tags from a string.
+
+        Parameters:
+            text (str): String containing potential HTML tags.
+
+        Returns:
+            str: String without HTML tags.
+        """
+        clean_text = re.sub(r'<.*?>', '', text)
+        return clean_text
+
+    def translate_unicode_characters(self, text):
+        """
+        Translate non-ASCII Unicode characters in a string to their closest ASCII representation.
+
+        Parameters:
+            text (str): String with potential Unicode characters.
+
+        Returns:
+            str: String with non-ASCII characters translated to ASCII.
+        """
+        translated_text = ''.join(
+            char if ord(char) < 128 else unicodedata.normalize(
+                'NFKD', char).encode('ascii', 'ignore').decode('utf-8')
+            for char in text
+        )
+        return translated_text
+
+    def detect_language(self, text):
+        """Detect the language of the text.
+
+        Parameters:
+            text (str): The text whose language is to be detected.
+
+        Returns:
+            str: The language of the text.
+        """
+        try:
+            lang = detect(text)
+            if lang == 'en':
+                return 'English'
+            elif lang == 'de':
+                return 'German'
+            else:
+                return 'Other'
+        except Exception as e:
+            print(f"Error detecting language. Error: {e}")
+            return 'Unknown'
+    
+    def transform_job_listing(self, job):
+        """
+        Transform a single job listing's data format.
+
+        Parameters:
+            job (dict): Raw job data.
+
+        Returns:
+            dict: Transformed job data.
+        """
+        job_title = job.get("name", "").lower()
+        job_level = "Middle"
+
+        if "senior" in job_title:
+            job_level = "Senior"
+        elif "junior" in job_title:
+            job_level = "Junior"
+        elif "intern" in job_title or "internship" in job_title:
+            job_level = "Intern"
+        elif "student" in job_title or "working student" in job_title:
+            job_level = "Student"
+        elif "lead" in job_title:
+            job_level = "Lead"
+        elif "head" in job_title:
+            job_level = "Head"
+
+        job_description = self.strip_html_tags(job.get("contents", ""))
+        language = self.detect_language(job_description)
+
+        transformed_job = {
+            #FIELDS["number"]: f"themuse-{self.job_number}",
+            FIELDS["title"]: job.get("name", ""),
+            FIELDS["company_name"]: job.get("company", {}).get("name", ""),
+            FIELDS["location"]: job.get("locations", [{}])[0].get("name", "") if job.get("locations") else "",
+            FIELDS["search_keyword"]: job.get("categories", [{}])[0].get("name", "") if job.get("categories") else "",
+            FIELDS["level"]: job_level,  
+            FIELDS["publish_date"]: job.get("publication_date", ""),
+            FIELDS["url"]: job.get("refs", {}).get("landing_page", ""),
+            FIELDS["search_datetime"]: datetime.now().isoformat(),
+            FIELDS["description"]: job_description,
+            FIELDS["language"]: language  
+        }
+        return transformed_job
+
+    def transform_jobs(self):
+        """
+        Load raw job data from a JSON file, transform its format, and save the transformed data to another JSON file.
+        """
+        with open(f"{self.directory_path}/themuse_raw_data.json", 'r') as raw_file:
+            raw_jobs = json.load(raw_file)
+
+        transformed_jobs = []
+        for job in raw_jobs:
+            job_location = job.get("locations", [{}])[0].get("name", "")
+            if "Germany" in job_location:
+                job["contents"] = self.translate_unicode_characters(job.get("contents", ""))
+                transformed_job = self.transform_job_listing(job)
+                transformed_jobs.append(transformed_job)
+                self.job_number += 1
+
+        with open(f"{self.processed_directory_path}/themuse_cleaned_data.json", 'w') as json_file:
+            json.dump(transformed_jobs, json_file, indent=4)
+
+# Main execution
+if __name__ == "__main__":
+    transformer = ThemuseDataTransformer()
+    transformer.transform_jobs()
