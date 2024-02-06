f4bceccfbf9d66cbd3901905c7ea3b5cef64a71b
Eugen Panov <egosign@gmail.com>
2024-02-06 11:03:48 +0100

    extract classes into separate files

--- airflow/dags/common/JobListings/spark/transform_jobs.py
+++ airflow/dags/common/JobListings/spark/transform_jobs.py
@@ -1,13 +1,17 @@
 import helper_transform as HelperTransform
 from constants import JOB_LEVELS
+from spark_session_manager import SparkSessionManager
+from s3_client_manager import S3ClientManager
+from file_processing import FileProcessing
+from data_storage import DataStorage
+
 
 import pyspark.sql.functions as F
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, TimestampType
 from pyspark.sql.functions import udf, col
 import os
-from io import BytesIO
-from datetime import datetime, timedelta
-from pyspark.sql import SparkSession
+
+
 # from airflow.exceptions import AirflowFailException
 import logging
 import sys
@@ -25,135 +29,6 @@ import hashlib
 # BUCKET_FROM = 'bronze'
 # BUCKET_TO = 'silver'
 # DELTA_MINUTES = 300
-
-
-class SparkSessionManager:
-    def __init__(self, appname):
-        self.appname = appname
-        self.spark_session = None
-
-    def get_spark_session(self):
-        if self.spark_session is None:
-            self.spark_session = SparkSession.builder.appName(
-                self.appname).getOrCreate()
-        return self.spark_session
-
-    def stop_spark_session(self):
-        if self.spark_session is not None:
-            self.spark_session.stop()
-            self.spark_session = None
-
-    def start(self):
-        self.get_spark_session()
-
-    def stop(self):
-        self.stop_spark_session()
-
-
-class S3ClientManager:
-    def __init__(self, acces_key, secret_key, endpoint_url):
-        # self.AWS_SPARK_ACCESS_KEY = os.getenv('MINIO_SPARK_ACCESS_KEY')
-        # self.AWS_SPARK_SECRET_KEY = os.getenv('MINIO_SPARK_SECRET_KEY')
-        # self.MINIO_IP_ADDRESS = socket.gethostbyname("minio")
-        self.acces_key = acces_key
-        self.secret_key = secret_key
-        self.endpoint_url = endpoint_url
-
-    def get_boto_client(self):
-        try:
-            return boto3.client(
-                's3',
-                aws_access_key_id=self.acces_key,
-                aws_secret_access_key=self.secret_key,
-                endpoint_url=self.endpoint_url
-            )
-        except Exception as e:
-            logging.error(f"Error creating S3 client: {str(e)}")
-            raise
-
-
-class FileProcessing:
-    def __init__(self, s3_client, bucket_name, source_name, delta_minutes):
-        self.s3_client = s3_client
-        self.bucket_name = bucket_name
-        self.source_name = source_name
-        self.delta_minutes = delta_minutes
-
-    def load_recent_files(self):
-        return self.merge_recent_files_to_df(self.bucket_name, self.source_name)
-
-    def is_file_recent(self, filename):
-        now = datetime.now()
-        hoursdelta = now - timedelta(minutes=self.delta_minutes)
-        try:
-            # filename format should be 'source_raw_datetime_location_keyword'
-            filename_splitted = filename.split('_')
-            date_str = filename_splitted[2]
-            # this is isoformat without special characters "-:"
-            file_datetime = datetime.strptime(date_str, '%Y%m%dt%H%M%S%f')
-            return file_datetime >= hoursdelta
-        except (IndexError, ValueError):
-            return False
-
-    def get_recent_files_from_s3(self, bucket_name, source_name):
-        # s3_client = self.get_boto_client()
-
-        all_files = self.s3_client.list_objects_v2(
-            Bucket=bucket_name, Prefix=F"{source_name}_raw")
-        # if len(all_files) == 0:
-        #     raise AirflowFailException(
-        #         'Aborting DAG run due to no files fetched.')
-
-        file_count = len(all_files.get('Contents', []))
-        logging.info(f"Total number of files: {file_count}")
-
-        # Filter for recent files
-        recent_files = [file['Key'] for file in all_files.get(
-            'Contents', []) if self.is_file_recent(file['Key'])]
-
-        file_count = len(recent_files)
-        logging.info(f"Total number of recent files: {file_count}")
-
-        return recent_files
-
-    def merge_recent_files_to_df(self, bucket_name, source_name):
-        # s3_client = self.get_boto_client()
-        recent_files = self.get_recent_files_from_s3(bucket_name, source_name)
-        df_list = []
-
-        for file_key in recent_files:
-            response = self.s3_client.get_object(
-                Bucket=bucket_name, Key=file_key)
-            file_content = response['Body'].read()
-            df = pd.read_csv(BytesIO(file_content))
-            # logging.info(f"Columns in file {file_key}: {df.columns.tolist()}")
-            df_list.append(df)
-
-        concatenated_df = pd.concat(df_list, ignore_index=True)
-        # logging.info(concatenated_df.columns)
-        return concatenated_df
-
-    # this is optimized for only one file
-    # def merge_recent_files_to_df(bucket_name, source_name):
-    #     s3_client = get_boto_client()
-    #     recent_files = get_recent_files_from_s3(bucket_name, source_name)
-    #     response = s3_client.get_object(Bucket=bucket_name, Key=recent_files[0])
-    #     file_content = response['Body'].read()
-    #     df = pd.read_csv(BytesIO(file_content))
-    #     logging.info(f"Columns in file {recent_files[0]}: {df.columns.tolist()}")
-    #     # df = df.drop("Unnamed: 0", axis=1)
-    #     return df
-
-
-class DataStorage:
-    def save_to_delta(self, df, target_path):
-        # overwrite: overwrite mode is important, otherwise errors
-        # mergeSchema: because still in development and otherwise error
-        df.write.option("mergeSchema", "true").format(
-            "delta").mode("overwrite").save(target_path)
-
-    def save_as_csv(self, df, target_path):
-        df.write.csv(target_path, mode="overwrite", header=True)
 
 
 class DataTransformation:
