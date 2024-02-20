4df67e177cca94cf7f5432181019db56fee1e2d6
Eugen Panov <egosign@gmail.com>
2024-02-20 16:12:52 +0100

    Refactor JobConfigManager class and fix load_config method

--- airflow/dags/common/JobListings/job_config_manager.py
+++ airflow/dags/common/JobListings/job_config_manager.py
@@ -6,12 +6,12 @@ class JobConfigManager:
     def __init__(self, file_name) -> None:
         self.file_name = file_name
 
-    @staticmethod
-    def load_config(file_name, config_part):
+    # @staticmethod
+    def load_config(self, config_part):
         """Lädt Konfiguration aus einer YAML-Datei."""
         dir_path = os.path.dirname(os.path.abspath(__file__))
-        config_file_path = os.path.join(dir_path, file_name)
-        with open(config_file_path, 'r') as file:
+        config_file_path = os.path.join(dir_path, self.file_name)
+        with open(config_file_path, "r") as file:
             config = yaml.safe_load(file)
 
         return config[config_part]
@@ -22,5 +22,5 @@ class JobConfigManager:
 
     def get_all_fact_table_columns(self):
         fact_info = self.load_config("fact_info")
-        factTable = fact_info['fctJobListings']
-        return factTable['dimKeyColumns'] + factTable['otherColumns']
+        factTable = fact_info["fctJobListings"]
+        return factTable["dimKeyColumns"] + factTable["otherColumns"]
