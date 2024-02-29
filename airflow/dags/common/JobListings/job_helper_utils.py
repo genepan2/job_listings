f847dd2ed8dd4ea3503fdccc580141ab42b79359
Eugen Panov <egosign@gmail.com>
2024-02-29 18:41:42 +0100

    Remove unused imports and functions

--- airflow/dags/common/JobListings/job_helper_utils.py
+++ airflow/dags/common/JobListings/job_helper_utils.py
@@ -1,12 +1,12 @@
 import os
 from datetime import datetime
 import re
-from pyspark.sql import SparkSession
+# from pyspark.sql import SparkSession
 
 # from pyspark.sql import functions as F
 from stringcase import snakecase, camelcase, pascalcase
 
-from job_config_constants import PATH, COLLECTIONS
+from job_config_constants import PATH
 
 
 def generate_id_column_name(table_name):
@@ -39,8 +39,6 @@ def generate_fct_key_column_name(dim_table_name):
     return dim_table_name
 
 
-# i want to generate the dim tabel name from the dim_tanle_id_column_name
-# and when the dim_table_id_column_name ends with "y" i want to remove the "y" and add "ies" to the end
 def generate_dim_table_name_from_id_column_name(dim_table_id_column_name):
     # Remove "Id" at the end and add "dim" at the start for general case
     if dim_table_id_column_name.endswith("_id"):
@@ -62,11 +60,6 @@ def generate_bridge_key_column_name(bridge_table_name):
     return bridge_table_name
 
 
-def get_collection_keys_without_all():
-    filtered_keys = [key for key in COLLECTIONS.keys() if key != "all"]
-    return filtered_keys
-
-
 def construct_file_path_for_data_source(source_name):
     return os.path.join(
         PATH["data_processed"],
@@ -78,10 +71,10 @@ def construct_file_path_for_data_source(source_name):
 def create_key_name(
     source_name, is_raw=True, search_location=None, search_keyword=None
 ):
-    now = sanitize_filename(datetime.now().isoformat(), True)
-    location = sanitize_filename(search_location) if search_location else ""
-    keyword = sanitize_filename(search_keyword) if search_keyword else ""
-    source = sanitize_filename(source_name)
+    now = sanitize_string(datetime.now().isoformat(), True)
+    location = sanitize_string(search_location) if search_location else ""
+    keyword = sanitize_string(search_keyword) if search_keyword else ""
+    source = sanitize_string(source_name)
 
     data_state = "raw" if is_raw else "cleaned"
 
@@ -89,45 +82,13 @@ def create_key_name(
     return "_".join(part for part in parts if part)
 
 
-def sanitize_filename(string, replace=False):
+def sanitize_string(string, replace=False):
     # note the backslash in front of the "-". otherwise it means from to.
     pattern = "[,!.\-: ]"
-    # logging.info(string)
+
     if replace is False:
         filename = re.sub(pattern, "_", string)
     else:
         filename = re.sub(pattern, "", string)
 
     return filename.lower().replace("__", "_")
-
-
-def get_spark_session(appname, endpoint_url, access_key, secret_key):
-    spark = (
-        SparkSession.builder.appName(appname)
-        .config("spark.network.timeout", "10000s")
-        #  .config("hive.metastore.uris", hive_metastore)
-        #  .config("hive.exec.dynamic.partition", "true")
-        #  .config("hive.exec.dynamic.partition.mode", "nonstrict")
-        .config("spark.sql.sources.partitionOverwriteMode", "dynamic")
-        .config("spark.hadoop.fs.s3a.multiobjectdelete.enable", "true")
-        .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-        .config("spark.hadoop.fs.s3a.fast.upload", "true")
-        .config("spark.hadoop.fs.s3a.endpoint", endpoint_url)
-        .config("spark.hadoop.fs.s3a.access.key", access_key)
-        .config("spark.hadoop.fs.s3a.secret.key", secret_key)
-        .config("spark.hadoop.fs.s3a.path.style.access", "true")
-        .config("spark.history.fs.logDirectory", "s3a://spark/")
-        .config("spark.sql.files.ignoreMissingFiles", "true")
-        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
-        .config(
-            "spark.sql.catalog.spark_catalog",
-            "org.apache.spark.sql.delta.catalog.DeltaCatalog",
-        )
-        .config(
-            "spark.delta.logStore.class",
-            "org.apache.spark.sql.delta.storage.S3SingleDriverLogStore",
-        )
-        #  .enableHiveSupport()
-        .getOrCreate()
-    )
-    return spark
