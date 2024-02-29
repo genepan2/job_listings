e790d1239bc03511f1fdc9a9cebd5e091473d692
Eugen Panov <egosign@gmail.com>
2024-02-29 18:42:12 +0100

    Add log level parameter to JobSparkSessionManager constructor

--- airflow/dags/common/JobListings/job_spark_session_manager.py
+++ airflow/dags/common/JobListings/job_spark_session_manager.py
@@ -2,14 +2,20 @@ from pyspark.sql import SparkSession
 
 
 class JobSparkSessionManager:
-    def __init__(self, appname):
+    def __init__(self, appname, log_level=None):
         self.appname = appname
         self.spark_session = None
+        self.log_level = log_level
 
     def get_spark_session(self):
         if self.spark_session is None:
             self.spark_session = SparkSession.builder.appName(
-                self.appname).getOrCreate()
+                self.appname
+            ).getOrCreate()
+
+            if self.log_level is not None:
+                self.spark_session.sparkContext.setLogLevel(self.log_level)
+
         return self.spark_session
 
     def stop_spark_session(self):
